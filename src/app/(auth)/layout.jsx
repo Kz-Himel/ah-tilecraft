4e1813cdd2a4fc1f8c886662b8c82534057a6e95
Kz-Himel <kzhimel129@gmail.com>
2026-05-03 21:12:00 +0600

    Start implement authentication in my webpage

---
+++ src/app/(auth)/layout.jsx
@@ -0,0 +1,15 @@
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import React from 'react';
+
+const AuthLayout = ({ children }) => {
+    return (
+        <div>
+            <Navbar />
+            {children}
+            <Footer />
+        </div>
+    );
+};
+
+export default AuthLayout;

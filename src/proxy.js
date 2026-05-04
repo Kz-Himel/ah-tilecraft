aae305d52032f5776d8984c87e8fe385706b4b7f
Kz-Himel <kzhimel129@gmail.com>
2026-05-04 20:15:46 +0600

    Add Proxy.js for protected routs

---
+++ src/proxy.js
@@ -0,0 +1,31 @@
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+
+export async function proxy(request) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  const { pathname } = request.nextUrl;
+
+  const authRoutes = ["/login", "/register"];
+
+  if (session && authRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL("/profile", request.url));
+  }
+
+  const isProfilePage = pathname === "/profile";
+
+  const isProductDetailPage =
+    pathname.startsWith("/products/") && pathname !== "/products";
+
+  if (!session && (isProfilePage || isProductDetailPage)) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/alltiles/:path*", "/profile", "/login", "/register"],
+};
